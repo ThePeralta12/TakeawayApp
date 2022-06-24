e16592788209ec2455c1f1f12788f5664793ba2c
Carl Ferdinard Peralta <44725129+ThePeralta12@users.noreply.github.com>
2022-06-24 16:48:50 +0100

    This the creation of a new project.

---
+++ app/src/main/java/com/example/takeawayapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.takeawayapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
